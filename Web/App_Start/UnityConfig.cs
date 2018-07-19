132d6f26396c1b73c8423777401f4ea5c83e3a66
Jamie Michalski <djamiem@gmail.com>
2018-07-19 12:01:10 -0400

    removing comment

--- Web/App_Start/UnityConfig.cs
+++ Web/App_Start/UnityConfig.cs
@@ -10,11 +10,6 @@ namespace Web
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-
-            // e.g. container.RegisterType<ITestService, TestService>();
             container.RegisterType<IOrderService, OrderService>();
 
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
