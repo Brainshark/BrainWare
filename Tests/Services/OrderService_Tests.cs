34e2274b64b9b6a8d23279402d5c1b689c2432c6
Jamie Michalski <djamiem@gmail.com>
2018-07-19 16:00:36 -0400

    Adding acceptance tests for order service

---
+++ Tests/Services/OrderService_Tests.cs
@@ -0,0 +1,29 @@
+﻿using Data.Services;
+using NUnit.Framework;
+
+namespace Tests.Services
+{
+    [TestFixture]
+    public class OrderService_Tests
+    {
+        private IDbService _dbService;
+
+        public OrderService_Tests()
+        {
+            _dbService = new DbService();
+        }
+
+        public void GetOrdersForCompany_ReturnsExpectedOrder()
+        {
+            // GIVEN I have an order
+            const string companyInsertSql = @"INSERT INTO company (name, company_id)
+            VALUES ('test name', 1)";
+
+            _dbService.ExecuteNonQuery(companyInsertSql);
+
+            // WHEN I get that order
+
+            // THEN I receive the order I expect
+        }
+    }
+}
